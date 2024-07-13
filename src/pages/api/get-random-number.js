7e4c8611a4b545425ab9ca293cdd042e227fd928
Murata <tomoya49m@gmail.com>
2024-07-13 23:37:31 +0900

    link

--- src/pages/api/get-random-number.js
+++ src/pages/api/get-random-number.js
@@ -1,7 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async function handler(req, res) {
-  const response = await fetch('http://localhost:5000/api/random-number');
-  const data = await response.json();
-  res.status(200).json(data);
+  try {
+      const response = await fetch('https://glsr-mura-tomos-projects.vercel.app/api/random-number');
+      const data = await response.json();
+      res.status(200).json(data);
+  } catch (error) {
+      console.error('Fetch error:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+  }
 }
